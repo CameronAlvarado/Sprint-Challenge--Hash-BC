b696e865c26f694534c113c51ec145bc5000c892
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-02-14 10:34:46 -0800

    dont understand hashtable ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -11,23 +11,23 @@ def get_indices_of_item_weights(weights, length, limit):
     # find two items who's weights equal the limit
 
     # loop thtought the array of items
-    for w in weights:
-        i = weights.index(w)
-        current = weights[i]
-        if weights[i+1] is not False:
-            next_i = weights[i+1]
-            hash_table_insert(ht, current, next_i)
-        else:
-            hash_table_insert(ht, current, None)
-
     # for w in weights:
     #     i = weights.index(w)
     #     current = weights[i]
     #     if weights[i+1] is not False:
     #         next_i = weights[i+1]
-    #     # Return answer as a tuple, (heavier index, lighter index)
-    #         if current + next_i is limit:
-    #             return (weights.index(current), weights.index(next_i))
+    #         hash_table_insert(ht, current, next_i)
+    #     else:
+    #         hash_table_insert(ht, current, None)
+
+    for w in weights:
+        i = weights.index(w)
+        current = weights[i]
+        if weights[i+1] is not None:
+            next_i = weights[i+1]
+        # Return answer as a tuple, (heavier index, lighter index)
+            if current + next_i is limit:
+                return (weights.index(current), weights.index(next_i))
 
 
 def print_answer(answer):
